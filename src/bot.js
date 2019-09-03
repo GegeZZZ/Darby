a92628a5f6c351631aa2366f7c67709c5c1233e9
Asher Wright <sastreen@hotmail.com>
2019-09-03 18:55:50 -0400

    Add multiple responses cleanly for caps

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,11 @@
 
 'use strict'
 
+const path = require('path');
 const slack = require('slack')
 const config = require('./config')
+const _ = require('lodash')
+const fs = require('fs')
 
 const send_message = (text, channel) => {
   slack.chat.postMessage({
@@ -17,11 +20,15 @@ const send_message = (text, channel) => {
   })
 }
 
+// Done only once on startup. There's probably a better way than json (yaml maybe?)
+const RESPONSES_TO_CAPS_PATH = path.join('src', 'data', 'responses_to_caps.JSON');
+const RESPONSES_TO_CAPS = JSON.parse(fs.readFileSync(RESPONSES_TO_CAPS_PATH)).responses
+
 const respond_to_event = (event) => {
   const message = event.text
     
   if (event.bot_id == null && message === message.toUpperCase()) {
-    send_message('I HEAR YOU', event.channel)
+    send_message(_.sample(RESPONSES_TO_CAPS), event.channel)
   }
 }
 
