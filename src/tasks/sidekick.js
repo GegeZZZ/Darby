464344119b14885387e1638d4946212104a69ac2
Asher Wright <sastreen@hotmail.com>
2019-09-15 00:17:34 -0400

    Add sidekicks task

--- src/tasks/sidekick.js
+++ src/tasks/sidekick.js
@@ -7,3 +7,11 @@ const slackAction = require('../slack_action')
 darbyDb.getDmChannelForUser("UMX7Q9LFP", (dmChannelId) => {
     slackAction.sendMessage("TESTING OUT TASKS", dmChannelId)
 })
+
+// Steps for sidekicks
+
+// 1. Get all User Ids,  names and DM channels
+
+// 2. Copy user Ids and make pairs
+
+// 3. DM each pair and ask them to meet up? Or make a group chat?
