55d2588d6e873bd5cced6e46328ff98f403451cf
Mary Briskin <mary.briskin@shopify.com>
2019-09-22 21:45:00 -0400

    Only send sidekicks on Sunday

--- src/tasks/sidekick.js
+++ src/tasks/sidekick.js
@@ -5,23 +5,27 @@ const darby = require("../darby");
 const slackAction = require("../slack_action");
 const _ = require("lodash");
 
-darbyDb.getAllUserIds(_res => {
-  console.log(_res);
-  let notRes = _.shuffle(["UMX7Q9LFP", "UMZBQQ0KZ"]);
-  let userIdsLeftHalf = notRes.splice(0, notRes.length / 2);
-  let userIdsRightHalf = notRes;
+const today = new Date();
 
-  for (let i = 0; i < userIdsRightHalf.length; i++) {
-    slackAction.openDmWithUsers(
-      userIdsLeftHalf[i],
-      userIdsRightHalf[i],
-      dmChannelId => {
-        darby.sendSidekicksMessage(
-          userIdsLeftHalf[i],
-          userIdsRightHalf[i],
-          dmChannelId
-        );
-      }
-    );
-  }
-});
+if (today.getDay() === 0) {
+  darbyDb.getAllUserIds(_res => {
+    console.log(_res);
+    let notRes = _.shuffle(["UMX7Q9LFP", "UMZBQQ0KZ"]);
+    let userIdsLeftHalf = notRes.splice(0, notRes.length / 2);
+    let userIdsRightHalf = notRes;
+
+    for (let i = 0; i < userIdsRightHalf.length; i++) {
+      slackAction.openDmWithUsers(
+        userIdsLeftHalf[i],
+        userIdsRightHalf[i],
+        dmChannelId => {
+          darby.sendSidekicksMessage(
+            userIdsLeftHalf[i],
+            userIdsRightHalf[i],
+            dmChannelId
+          );
+        }
+      );
+    }
+  });
+}
