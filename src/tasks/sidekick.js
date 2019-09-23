b7db278c3ca1c6d6623f85c456541c5e9840c9f0
Mary Briskin <mary.briskin@shopify.com>
2019-09-22 22:00:40 -0400

    Day thing not working

--- src/tasks/sidekick.js
+++ src/tasks/sidekick.js
@@ -5,27 +5,26 @@ const darby = require("../darby");
 const slackAction = require("../slack_action");
 const _ = require("lodash");
 
-const today = new Date();
+console.log("starting sidekicks");
+console.log(`DAY: ${new Date().getDay()}`);
 
-if (today.getDay() === 0) {
-  darbyDb.getAllUserIds(_res => {
-    console.log(_res);
-    let notRes = _.shuffle(["UMX7Q9LFP", "UMZBQQ0KZ"]);
-    const userIdsLeftHalf = notRes.splice(0, notRes.length / 2);
-    const userIdsRightHalf = notRes;
+darbyDb.getAllUserIds(_res => {
+  console.log(_res);
+  let notRes = _.shuffle(["UMX7Q9LFP", "UMZBQQ0KZ"]);
+  const userIdsLeftHalf = notRes.splice(0, notRes.length / 2);
+  const userIdsRightHalf = notRes;
 
-    for (let i = 0; i < userIdsRightHalf.length; i++) {
-      slackAction.openDmWithUsers(
-        userIdsLeftHalf[i],
-        userIdsRightHalf[i],
-        dmChannelId => {
-          darby.sendSidekicksMessage(
-            userIdsLeftHalf[i],
-            userIdsRightHalf[i],
-            dmChannelId
-          );
-        }
-      );
-    }
-  });
-}
+  for (let i = 0; i < userIdsRightHalf.length; i++) {
+    slackAction.openDmWithUsers(
+      userIdsLeftHalf[i],
+      userIdsRightHalf[i],
+      dmChannelId => {
+        darby.sendSidekicksMessage(
+          userIdsLeftHalf[i],
+          userIdsRightHalf[i],
+          dmChannelId
+        );
+      }
+    );
+  }
+});
