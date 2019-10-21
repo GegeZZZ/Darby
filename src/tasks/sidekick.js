75be2422d083363bc3a4a6480d002b3500a128c1
Asher Wright <sastreen@hotmail.com>
2019-10-20 22:04:54 -0400

    Rig sidekicks for two people

--- src/tasks/sidekick.js
+++ src/tasks/sidekick.js
@@ -12,6 +12,25 @@ const today = new Date().getDay();
 if (today === 1) {
   darbyDb.getAllUserIds(res => {
     console.log(res);
+
+    // custom (fixed) matching for special request
+    const fixedIDA = 'UMZBQQ0KZ'
+    const fixedIDB = 'UMXFU3Q5B'
+
+    // Very messy, I'm in a rush here.
+    const idAIndex = res.indexOf('UMZBQQ0KZ');
+    if (idAIndex > -1) {
+      res.splice(idAIndex, 1);
+    }
+
+    const idBIndex = res.indexOf('UMXFU3Q5B');
+    if (idBIndex > -1) {
+      res.splice(idBIndex, 1);
+    }
+
+    openDmAndSendMessageToUsers(fixedIDA, fixedIDB)
+    // End of fixed matching
+
     let shuffledUserIds = _.shuffle(res);
     const userIdsLeftHalf = shuffledUserIds.splice(
       0,
@@ -20,17 +39,21 @@ if (today === 1) {
     const userIdsRightHalf = shuffledUserIds;
 
     for (let i = 0; i < userIdsRightHalf.length; i++) {
-      slackAction.openDmWithUsers(
-        userIdsLeftHalf[i],
-        userIdsRightHalf[i],
-        dmChannelId => {
-          darby.sendSidekicksMessage(
-            userIdsLeftHalf[i],
-            userIdsRightHalf[i],
-            dmChannelId
-          );
-        }
-      );
+      openDmAndSendMessageToUsers(userIdsLeftHalf[i], userIdsRightHalf[i])
     }
   });
+}
+
+function openDmAndSendMessageToUsers(userIdA, userIdB){
+  slackAction.openDmWithUsers(
+    userIdA,
+    userIdB,
+    dmChannelId => {
+      darby.sendSidekicksMessage(
+        userIdA,
+        userIdB,
+        dmChannelId
+      );
+    }
+  );
 }
