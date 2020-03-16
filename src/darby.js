3ff2eca68edeaeb3f62d9ed544d4ef8651b10678
Asher Wright <sastreen@hotmail.com>
2020-03-15 23:48:11 -0400

    Add sidekicks for Corona

--- src/darby.js
+++ src/darby.js
@@ -26,7 +26,7 @@ const ENCOURAGEMENT_MESSAGE_RESPONSES = JSON.parse(
   fs.readFileSync("src/responses/encouragement_message.JSON")
 );
 const SIDEKICKS_RESPONSES = JSON.parse(
-  fs.readFileSync("src/responses/sidekicks.JSON")
+  fs.readFileSync("src/responses/corona_virus_sidekicks.JSON")
 );
 const START_ODDS_RESPONSES = JSON.parse(
   fs.readFileSync("src/responses/odds_start_odds.JSON")
