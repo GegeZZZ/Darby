6d233110db9c833ae39bb3580c0184196c29d9e7
Asher Wright <asher.wright@shopify.com>
2020-08-14 21:39:38 -0300

    fix object key casing

--- src/end_of_year_video.js
+++ src/end_of_year_video.js
@@ -4,26 +4,26 @@ const slackAction = require("./slack_action");
 
 const PROMPT_TO_RESPONSES = {
     "hey darby, how did it all start?":                             "I will post a video here.",
-    "Nice view!":                                                   "Yeah - Maine was awesome!",
-    "Btw Darby, what’s your favorite food?":                        "Data :stuck_out_tongue:",
-    "Seriously, what actual food do you like?":                     "I will post a food video here.",
-    "Yummy… Darby what else did you do all day other than eat?":    "I will post a work video here.",
-    "It looks like your days weren’t sparse at all...":             "Yes! I was very busy. Class, homework, dates, friends... Plus I will post a video",
-    "Darby... :man-facepalming:":                                   "Sorry, wrong file!",
-    "Darby, I heard you are graduating today!":                     "Yes. And can I tell you a secret?",
-    "Sure.":                                                        "I am going to miss all my classmates a lot.",
-    "Thank you, Darby!":                                            "Wait...",
-    "What is it?":                                                  "This can't end yet...",
-    "?":                                                            "Without a special performance brought to you by MBAn!!!"
+    "nice view!":                                                   "Yeah - Maine was awesome!",
+    "btw darby, what’s your favorite food?":                        "Data :stuck_out_tongue:",
+    "seriously, what actual food do you like?":                     "I will post a food video here.",
+    "yummy... darby what else did you do all day other than eat?":  "I will post a work video here.",
+    "it looks like your days weren’t sparse at all...":             "Yes! I was very busy. Class, homework, dates, friends... Plus I will post a video",
+    "darby... :man-facepalming:":                                   "Sorry, wrong file!",
+    "darby, i heard you are graduating today!":                     "Yes. And can I tell you a secret?",
+    "sure.":                                                        "I am going to miss all my classmates a lot.",
+    "thank you, darby!":                                            "Wait...",
+    "what is it?":                                                  "This can't end yet...",
+    "?":                                                            "Without a special performance brought to you by the MBAn class of 2020!!!"
 }
 
 const PROMPT_TO_VIDEOS = {
-    "hey darby, how did it all start?": "TODO",
-    "Seriously, what actual food do you like?": "TODO",
-    "It looks like your days weren’t sparse at all...": "TODO",
-    "Darby... :man-facepalming:": "TODO",
-    "Sure.": "TODO",
-    "?": "TODO"
+    "hey darby, how did it all start?":                 "TODO",
+    "seriously, what actual food do you like?":         "TODO",
+    "it looks like your days weren’t sparse at all...": "TODO",
+    "darby... :man-facepalming:":                       "TODO",
+    "sure.":                                            "TODO",
+    "?":                                                "TODO"
 }
 
 function respondToEndOfYearVideo(text, channel) {
@@ -31,6 +31,10 @@ function respondToEndOfYearVideo(text, channel) {
 
     if (cleanedText in PROMPT_TO_RESPONSES) {
         slackAction.sendMessage(PROMPT_TO_RESPONSES[cleanedText], channel, false)
+    }
+
+    if (cleanedText in PROMPT_TO_VIDEOS) {
+        slackAction.sendMessage(PROMPT_TO_VIDEOS[cleanedText], channel, false)
     }
 }
 
